db7d5e590f95533df925d9d48e5270aa74cd09bf
Bobby Chisholm <rwc1007@wildcats.unh.edu>
2019-09-20 13:14:01 -0400

    Bobby-Created singleton class to track document frequencies

--- src/main/java/lucene/DocumentFreqTracker.java
+++ src/main/java/lucene/DocumentFreqTracker.java
@@ -3,8 +3,8 @@ package lucene;
 import java.util.HashMap;
 
 /**
- * This class is a singleton that is used ti keep track of 
- * @author Bobby
+ * This class is a singleton that is used to keep track of document frequency
+ * @author Bobby Chisholm
  *
  */
 public class DocumentFreqTracker {
@@ -12,18 +12,32 @@ public class DocumentFreqTracker {
 	private static HashMap<String, Integer> docFreq;
 	
 	private DocumentFreqTracker() {
-		
+		docFreq = new HashMap<String, Integer>();
 	}
 
+	/**
+	 * Singleton get method to create new instance or get existing
+	 * @return
+	 */
 	public static DocumentFreqTracker getInstance() {
 		if( instance == null) {
 			instance = new DocumentFreqTracker();
-			docFreq = new HashMap<String, Integer>();
 		}
 		return instance;
 	}
 	
+	/**
+	 * If the key doesn't exist it adds a new one, if it does adds 1 to count
+	 * @param str key to be checked/added
+	 */
 	protected static void addString(String str) {
-		
+		Integer count = docFreq.get(str);
+		if(count == null) {
+			docFreq.put(str,  1);
+		} else count += 1;
+	}
+	
+	protected static HashMap<String, Integer> getDocFreqs(){
+		return docFreq;
 	}
 }
