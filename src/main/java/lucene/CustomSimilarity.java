f2110159be8fd529c06e78ab125a11edf916ec72
Bobby Chisholm <rwc1007@wildcats.unh.edu>
2019-09-20 12:45:57 -0400

    Bobby- Starting to implement the TF-IDF models for ranking

--- src/main/java/lucene/CustomSimilarity.java
+++ src/main/java/lucene/CustomSimilarity.java
@@ -9,11 +9,14 @@ import org.apache.lucene.search.similarities.SimilarityBase;
  * Creates a custom similarity/scoring function and returns it
  */
 public class CustomSimilarity {
+	private static String tfidfModel = null;
 	/**
-	 * Returns a custom scoring function
-	 * @return a custom similarity
+	 * Returns a a TF-IDF lnc.ltn ranking model to be used 
+	 * as a custom similarity when ranking documents
+	 * @return an lnc.ltn similarity
 	 */
-	public static Similarity getSimilarity() {
+	public static Similarity lncltn() {
+		tfidfModel = "lnc.ltn";
         SimilarityBase mySimilarity = new SimilarityBase() {
         	@Override
             protected float score(BasicStats basicStats, float v, float v1) {
@@ -22,7 +25,51 @@ public class CustomSimilarity {
 
             @Override
             public String toString() {
-                return "CustomSimilarity";
+                return "lnc.ltn";
+            }
+        };
+
+        return mySimilarity;
+    }
+	
+	/**
+	 * Returns a a TF-IDF bnn.bnn ranking model to be used 
+	 * as a custom similarity when ranking documents
+	 * @return an bnn.bnn similarity
+	 */
+	public static Similarity bnnbnn() {
+		tfidfModel = "bnn.bnn";
+        SimilarityBase mySimilarity = new SimilarityBase() {
+        	@Override
+            protected float score(BasicStats basicStats, float v, float v1) {
+                return v;
+            }
+
+            @Override
+            public String toString() {
+                return "bnn.bnn";
+            }
+        };
+
+        return mySimilarity;
+    }
+	
+	/**
+	 * Returns a a TF-IDF anc.apc ranking model to be used 
+	 * as a custom similarity when ranking documents
+	 * @return an anc.apc similarity
+	 */
+	public static Similarity ancapc() {
+		tfidfModel = "anc.apc";
+        SimilarityBase mySimilarity = new SimilarityBase() {
+        	@Override
+            protected float score(BasicStats basicStats, float v, float v1) {
+                return v;
+            }
+
+            @Override
+            public String toString() {
+                return "anc.apc";
             }
         };
 
@@ -31,9 +78,10 @@ public class CustomSimilarity {
 	
 	/**
 	 * Returns the name of the ranking function
+	 * null if a similarit has not been selected yet
 	 */
 	public static String getSimilarityName() {
-		return "CustomSimilarity";
+		return tfidfModel;
 		
 	}
 }
